15b648cf18048e223c3a9b8a2a3bdc4190bd8fb6
phanindra27 <phaniv16@gmail.com>
2018-05-22 12:29:46 -0400

    Update default.rb

--- default.rb
+++ default.rb
@@ -1,22 +1,53 @@
-
-login file
-
-
 {
-"accessKeyId":"your access key",
-"secretAccessKey":"secret key",
-"httpProxy":"http://one.proxy.att.com:8080",
-"httpsProxy":"https://one.proxy.att.com:8080",
-"fs.s3a.proxy.host":"one.proxy.att.com",
-"fs.s3a.proxy.port":"8080",
-"region":"us-east-1",
-"userName":"username",
-"roleArn":"role arn",
-"roleExpireSeconds":"21600",
-"kmsKeyArn" : "your key"
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstances",
+                "ec2:DescribeImages",
+                "ec2:DescribeKeyPairs",
+                "ec2:DescribeVpcs",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "ec2:Describe*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ec2:RunInstances",
+            "Resource": [
+                "arn:aws:ec2:us-east-1:990300515209:network-interface/*",
+                "arn:aws:ec2:us-east-1:990300515209:volume/*",
+                "arn:aws:ec2:us-east-1:990300515209:key-pair/*",
+                "arn:aws:ec2:us-east-1:990300515209:security-group/*",
+                "arn:aws:ec2:us-east-1:990300515209:subnet/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ec2:RunInstances",
+            "Resource": [
+                "arn:aws:ec2:us-east-1:990300515209:instance/*"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "ec2:InstanceType": "t2.micro"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ec2:RunInstances",
+            "Resource": [
+                "arn:aws:ec2:us-east-1::image/ami-14c5486b"
+            ],
+            "Condition": {
+                "StringEquals": {
+                    "ec2:Owner": "amazon"
+                }
+            }
+        }
+    ]
 }
-
-commands---
-aws s3 cp sample.txt s3://bucketname/dl/datalibrary/ --sse aws:kms --sse-kms-key-id (kms key arn) --profile profile-name
-
-aws s3 rm s3://somerandombucket123/1/account/sample.txt --profile profile-name
