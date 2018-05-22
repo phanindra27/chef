54422b26919e8a098178e9a57d79c573fe43e7c8
phanindra27 <phaniv16@gmail.com>
2018-05-22 11:58:00 -0400

    Update default.rb

--- default.rb
+++ default.rb
@@ -1,20 +1,22 @@
-#
-# Cookbook Name:: localusers
-# Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 
-search(:users, "*:*").each do |data|
-       user data["id"]do
-       comment data["comment"]
-       uid data["uid"]
-       gid data["gid"]
-       home data["home"]
-       shell data["shell"]
-  
-     end
-end
-include_recipe "localusers::groups"
+login file
+
+
+{
+"accessKeyId":"your access key",
+"secretAccessKey":"secret key",
+"httpProxy":"http://one.proxy.att.com:8080",
+"httpsProxy":"https://one.proxy.att.com:8080",
+"fs.s3a.proxy.host":"one.proxy.att.com",
+"fs.s3a.proxy.port":"8080",
+"region":"us-east-1",
+"userName":"username",
+"roleArn":"role arn",
+"roleExpireSeconds":"21600",
+"kmsKeyArn" : "your key"
+}
+
+commands---
+aws s3 cp sample.txt s3://bucketname/dl/datalibrary/ --sse aws:kms --sse-kms-key-id (kms key arn) --profile profile-name
+
+aws s3 rm s3://somerandombucket123/1/account/sample.txt --profile profile-name
